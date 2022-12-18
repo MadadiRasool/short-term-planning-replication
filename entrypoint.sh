2f4e21ae33b72de514bac43b5a400d9ccfe0dbc1
rm <madadi.rasool@yahoo.com>
2022-12-18 18:51:16 +0330

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
-python3 print(test)
-# python scripts/03-estimate-baseline-models.py --model canonical_NK --nprocs 4
+#!/bin/bash
+python print("Start Get Data ... ")
+python scripts/01-construct-estimation-data.py
 
+python print("Start Create Fortran Models")
+python scripts/02-construct-fortran-models.py
 
-python print("Hello World")
+python print("Start Create BaseLine Models")
+python scripts/03-estimate-baseline-models.py --model canonical_NK --nprocs 4
