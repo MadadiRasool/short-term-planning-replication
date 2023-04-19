af6a4da0874859a732518a671433029349d653ce
rm <madadi.rasool@yahoo.com>
2023-04-19 13:43:08 +0330

    change 6

--- scripts_ir/models.py
+++ scripts_ir/models.py
@@ -23,7 +23,7 @@ class ModelAttributes(object):
                  "yaml_file",
                  "fortran_directory",
                  "fixed_parameters",
-                 "model", 
+                 "model",
                  "smc_file")
 
     estimation_settings = {"nphi": 500,
@@ -31,15 +31,13 @@ class ModelAttributes(object):
                            "npart": 16000,
                            "pe": 1000}
 
-
-
     def __init__(self, name, yaml_file, fortran_directory, fixed_parameters={}, estimation_settings={}, smc_file=None):
         self.name = name
         self.yaml_file = yaml_file
         self.fortran_directory = fortran_directory
         self.fixed_parameters = fixed_parameters
         self.estimation_settings.update(estimation_settings)
-        self.smc_file = smc_file 
+        self.smc_file = smc_file
 
     def load(self, add_shocks=False, add_hats=False):
         self.model = DSGE.read(self.yaml_file)
@@ -49,20 +47,22 @@ class ModelAttributes(object):
             v_epsxi = Variable('v_epsxi')
             v_epsi = Variable('v_epsi')
             v_epsy = Variable('v_epsy')
-            self.model['var_ordering'] = self.model['var_ordering'] + [v_epsxi, v_epsi, v_epsy]
-            self.model['perturb_eq'] = (self.model['perturb_eq'] + 
+            self.model['var_ordering'] = self.model['var_ordering'] + \
+                [v_epsxi, v_epsi, v_epsy]
+            self.model['perturb_eq'] = (self.model['perturb_eq'] +
                                         [Equation(v_epsxi, self.model.shocks[0]),
-                                         Equation(v_epsi, self.model.shocks[1]),
+                                         Equation(
+                                             v_epsi, self.model.shocks[1]),
                                          Equation(v_epsy, self.model.shocks[2])])
 
         if add_hats:
             v_yhat = Variable('yhat')
             v_pihat = Variable('pihat')
-            self.model['var_ordering'] = self.model['var_ordering'] + [v_yhat, v_pihat]
-            self.model['perturb_eq'] = (self.model['perturb_eq'] + 
-                                           [Equation(v_yhat, self.model.variables[0] - self.model.variables[3]),
+            self.model['var_ordering'] = self.model['var_ordering'] + \
+                [v_yhat, v_pihat]
+            self.model['perturb_eq'] = (self.model['perturb_eq'] +
+                                        [Equation(v_yhat, self.model.variables[0] - self.model.variables[3]),
                                             Equation(v_pihat, self.model.variables[1] - self.model.variables[4])])
-
 
     def parameters(self):
         return [str(para) for para in self.model.parameters]
@@ -70,20 +70,18 @@ class ModelAttributes(object):
     def linear_model(self):
         return self.model.compile_model()
 
-   
-
     def create_fortran_model(self):
-        if self.smc_file is None:                                 
+        if self.smc_file is None:
             smc_file = smc(self.model)
         else:
-            smc_file = self.smc_file 
+            smc_file = self.smc_file
 
-        model_linear = self.model.compile_model()                 
-        other_files = {'data.txt': model_linear.yy,          
-                       'prior.txt': 'prior.txt'}             
-        make_smc(smc_file, other_files=other_files,          
-                 output_directory=self.fortran_directory)                      
-        write_prior_file(model_linear.prior, self.fortran_directory)           
+        model_linear = self.model.compile_model()
+        other_files = {'data.txt': model_linear.yy,
+                       'prior.txt': 'prior.txt'}
+        make_smc(smc_file, other_files=other_files,
+                 output_directory=self.fortran_directory)
+        write_prior_file(model_linear.prior, self.fortran_directory)
 
     def estimate(self, **kwargs):
         smc = SMCDriver('./' + self.fortran_directory + '/smc')
@@ -136,6 +134,10 @@ finite_horizon_ir = ModelAttributes(
         "phiyLR": "phiy",
         "alpha": 0.2,
     },
+    estimation_settings={"nphi": 500,
+                         "nblocks": 3,
+                         "npart": 16000,
+                         "pe": 2000}
 )
 
 
@@ -192,19 +194,19 @@ angeletos_lian = ModelAttributes(
     yaml_file='models/finite_horizon_angeletos_lian.yaml',
     fortran_directory='fortran/angeletos_lian',
     fixed_parameters={
-        "alpha":0.75,
-        "phipiLR":1.5, 
-        "phiyLR":0.25
+        "alpha": 0.75,
+        "phipiLR": 1.5,
+        "phiyLR": 0.25
     },
 )
-    
+
 habits = ModelAttributes(
     name="Hybrid NK",
     yaml_file='models/finite_horizon_habits_lam.yaml',
     fortran_directory='fortran/habits',
     fixed_parameters={
-        "alpha":0.75,
-        "lampi":0.75
+        "alpha": 0.75,
+        "lampi": 0.75
     },
 )
 
@@ -214,7 +216,7 @@ habits_lampi = ModelAttributes(
     yaml_file='models/finite_horizon_habits_lam.yaml',
     fortran_directory='fortran/habits_lampi',
     fixed_parameters={
-        "alpha":0.75,
+        "alpha": 0.75,
     },
 )
 
@@ -233,4 +235,3 @@ single_agent_models = [
         smc_file=single_agent_template.format(k=k)
     )
     for k in range(5)]
-
